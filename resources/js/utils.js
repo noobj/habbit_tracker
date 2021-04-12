bfa76f96555162943d3254dc713f1072f9138659
Bandy <jaky0089@outlook.com>
2021-04-12 15:42:14 +0800

    docs: add comment #7

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -7,10 +7,15 @@ import {
   format
 } from 'date-fns'
 
+// get start of today
 const endDate = startOfToday()
 let startDate = subYears(endDate, 1)
 startDate = subDays(startDate, 7)
 
+/**
+ * generate dates for render grid
+ * @returns {Date[]} dates - dates for render grid
+ */
 const getDates = () => {
   let dates = eachDayOfInterval({
       start: startDate,
@@ -18,13 +23,28 @@ const getDates = () => {
     })
     .sort((d1, d2) => d1 < d2 ? -1 : d1 > d2 ? 1 : 0)
   
+  // 剔除 dates 最前面非週日的日期
   dates = dates.slice(dates.indexOf(dates.find(d => isSunday(d))))
+  // 只保留 53 週
   if (dates.length / 7 > 53) {
     dates = dates.slice(7)
   }
   return dates
 }
 
+/**
+ * @typedef Summary
+ * @property {Number} id
+ * @property {String} date - ex: Apr 8, 2021
+ * @property {String} duration - ex: 5h28m
+ * @property {Number} level - 1 to 4
+ * @property {Number} timestamp - unix timestamp of date
+ */
+
+/**
+ * generate dates for render grid
+ * @returns {Promise<Summary[]>} dates - dates for render grid
+ */
 const getSummaries = async () => {
   let params = new URLSearchParams
   params.set('start_date', format(startDate, 'yyyy-MM-dd'))
